0563e62a088fe1be3d802da882341df19be05994
raychannudam <channudamray2002.rcu@gmail.com>
2022-07-20 11:37:39 +0700

    edited on view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,8 +3,35 @@
     <div class=" my-9">
         {{-- Hero --}}
         <div class="">
-            <div class="bg-[url('{{URL('storage/hero.png')}}')]">
-
+            <div>
+                
+            </div>
+        </div>
+        {{-- Cards --}}
+        <div class="w-full px-24">
+            <div class="flex flex-row space-x-36">
+                <div class="bg-white rounded-md w-1/2">
+                    <div class="flex flex-col items-center justify-center space-y-5 w-full p-10">
+                        <div class="flex flex-row space-x-3 items-center justify-center">
+                            <div>
+                                font
+                            </div>
+                            <div class="text-2xl">
+                                <h1>
+                                    Browse all local events in your community
+                                </h1>
+                            </div>
+                        </div>
+                        <div class="w-full">
+                            <div class="border p-3 rounded-md text-center text-xl">
+                                <a href="/">Browse</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="bg-white rounded-md w-1/2">
+                    World
+                </div>
             </div>
         </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     './resources/css/**/*.css',
   ],
   theme: {
-    extend: {}
+    extend: {
+      backgroundImage: {
+        // 'hero': "url('https://traconference.eu/wp-content/uploads/2021/08/pexels-luis-quintero-2833037-scaled-e1629113060469.jpg')",
+        'hero': "url('/public/storage/hero.png')",
+      },
+    }
   },
   variants: {},
   plugins: [
