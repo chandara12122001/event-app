121a8bf3dc11d810dfbaa5d2379326fd318f1d48
chandara12122001 <chandara12122001@gmail.com>
2022-08-06 15:39:06 +0700

    fixing error on all event and detaile vent

--- resources/views/event/show.blade.php
+++ resources/views/event/show.blade.php
@@ -2,8 +2,7 @@
 @section('content')
 {{-- Start event feature --}}
 <div class="mb-10">
-    {{-- <div style="background-image: url({{asset($event->images[0] ->image)}})" --}}
-        class="bg-cover bg-center pt-1/6">
+    <div style="background-image: url({{asset($event->images[0]->image)}})" class="bg-cover bg-center pt-1/6">
         <div class="bg-gradient-to-t from-gray-900 to-transparent w-full h-full">
             <div class="md:px-36 px-20 py-10 flex flex-col space-y-5">
                 <div class="text-3xl text-white font-bold">
@@ -18,23 +17,11 @@
 </div>
 {{-- End event feature --}}
 {{-- Start facilities --}}
-<<<<<<< HEAD
-<div class="m-10 px-56">
-    <div class="flex flex-row space-x-28 items-center justify-center text-xl">
+<div class="m-10 md:px-56">
+    <div class="flex md:flex-row md:space-x-28 md:space-y-0 items-center justify-center flex-col space-y-5">
         <div class="flex flex-col space-y-2 items-center justify-center">
             <div class="text-orange-400 text-2xl">
                 <i class="fa-solid fa-wifi"></i>
-=======
-    <div class="m-10 md:px-56">
-        <div class="flex md:flex-row md:space-x-28 md:space-y-0 items-center justify-center flex-col space-y-5">
-            <div class="flex flex-col space-y-2 items-center justify-center">
-                <div class="text-orange-400 text-2xl">
-                    <i class="fa-solid fa-wifi"></i>
-                </div>
-                <div>
-                    <p>WiFi Access</p>
-                </div>
->>>>>>> 48c93ae948f68424beb633f5f8ada041640f3a65
             </div>
             <div>
                 <p>WiFi Access</p>
@@ -83,51 +70,26 @@
     </div>
 </div> --}}
 
-<<<<<<< HEAD
 {{-- main-info section --}}
-<div class="container mx-auto mt-8">
+{{-- <div class="container mx-auto mt-8">
     <div class="">
         <h5 class="font-bold text-4xl">Description</h5>
-        <p class="mt-4 px-8">{{$event->description}}</p>
+        <p class="mt-4 px-8">{{$event->description}}</p> --}}
         {{-- End facilities --}}
         {{-- Start important info --}}
-        <div class="m-10 px-56">
-            <div class="flex flex-row space-x-28 border border-orange-400 rounded-md p-10 items-center justify-center">
+        <div class="m-10 md:px-56 px-20">
+            <div
+                class="flex md:flex-row md:space-x-28  md:space-y-0 flex-col space-y-10 border border-orange-400 rounded-md p-10 items-center justify-center">
                 <div class="flex flex-row space-x-5 text-orange-400 items-center justify-center text-xl">
+                    <div hidden id="eventID">
+                        {{$event->id}}
+                    </div>
                     <div>
                         <i class="fa-solid fa-location-dot"></i>
                     </div>
                     <div class="text-gray-900">
-                        {{-- <p>{{$event->location->name}}</p> --}}
+                        <p>{{$event->location->name}}</p>
                     </div>
-=======
-    {{-- main-info section --}}
-    {{-- <div class="container mx-auto mt-8">
-        <div class="">
-            <h5 class="font-bold text-4xl">Description</h5>
-            <p class="mt-4 px-8">{{$event->description}}</p> --}}
-{{-- End facilities --}}
-{{-- Start important info --}}
-    <div class="m-10 md:px-56 px-20">
-        <div class="flex md:flex-row md:space-x-28  md:space-y-0 flex-col space-y-10 border border-orange-400 rounded-md p-10 items-center justify-center">
-            <div class="flex flex-row space-x-5 text-orange-400 items-center justify-center text-xl">
-                <div hidden id="eventID">
-                    {{$event->id}}
-                </div>
-                <div>
-                    <i class="fa-solid fa-location-dot"></i>
-                </div>
-                <div class="text-gray-900">
-                    <p>{{$event->location->name}}</p>
-                </div>
-            </div>
-            <div class="flex flex-row space-x-5 text-orange-400 items-center justify-center text-xl">
-                <div>
-                    <i class="fa-solid fa-calendar-day"></i>
-                </div>
-                <div class="text-gray-900">
-                    <p>{{$event->event_date}}</p>
->>>>>>> 48c93ae948f68424beb633f5f8ada041640f3a65
                 </div>
                 <div class="flex flex-row space-x-5 text-orange-400 items-center justify-center text-xl">
                     <div>
@@ -147,10 +109,9 @@
                 </div>
             </div>
         </div>
-<<<<<<< HEAD
         {{-- End important info --}}
         {{-- Start description --}}
-        <div class="m-10 px-56">
+        <div class="m-10 md:px-56 px-20">
             <div class="flex flex-col space-y-6">
                 <div class="text-xl text-gray-800 font-bold">
                     <p>Description</p>
@@ -162,9 +123,9 @@
         </div>
         {{-- End description --}}
         {{-- Start detail info --}}
-        <div class="m-10 px-56">
-            <div class="flex flex-row space-x-10">
-                <div class="w-1/2">
+        <div class="m-10 md:px-56 px-20">
+            <div class="flex md:flex-row md:space-x-10 md:space-y-0 flex-col space-y-10">
+                <div class="md:w-1/2 w-full">
                     <div class="flex flex-col space-y-3">
                         <div class="w-full rounded-md text-center p-2 bg-red-400 text-white">
                             <div class="flex flex-row space-x-3 items-center justify-center text-lg">
@@ -174,34 +135,6 @@
                                 <div>
                                     <p>View on map</p>
                                 </div>
-=======
-    </div>
-{{-- End important info --}}
-{{-- Start description --}}
-    <div class="m-10 md:px-56 px-20">
-        <div class="flex flex-col space-y-6">
-            <div class="text-xl text-gray-800 font-bold">
-                <p>Description</p>
-            </div>
-            <div class="w-full text-justify">
-                <p>{{$event->description}}</p>
-            </div>
-        </div>
-    </div>
-{{-- End description --}}
-{{-- Start detail info --}}
-    <div class="m-10 md:px-56 px-20">
-        <div class="flex md:flex-row md:space-x-10 md:space-y-0 flex-col space-y-10">
-            <div class="md:w-1/2 w-full">
-                <div class="flex flex-col space-y-3">
-                    <div class="w-full rounded-md text-center p-2 bg-red-400 text-white">
-                        <div class="flex flex-row space-x-3 items-center justify-center text-lg">
-                            <div>
-                                <i class="fa-solid fa-map-location-dot"></i>
-                            </div>
-                            <div>
-                                <p>View on map</p>
->>>>>>> 48c93ae948f68424beb633f5f8ada041640f3a65
                             </div>
                         </div>
                         <div class="w-full rounded-md shadow-md">
@@ -209,8 +142,7 @@
                         </div>
                     </div>
                 </div>
-<<<<<<< HEAD
-                <div class="w-1/2">
+                <div class="md:w-1/2 w-full">
                     <div class="flex flex-col space-y-5">
                         <div class="flex flex-col border border-green-400 rounded-md w-full h-1/2">
                             <div class="flex flex-row space-x-3 p-3 w-full items-center">
@@ -224,22 +156,6 @@
                                     <div class=" bg-green-400 rounded-md p-2">
                                         <p>Organizer</p>
                                     </div>
-=======
-            </div>
-            <div class="md:w-1/2 w-full">
-                <div class="flex flex-col space-y-5">
-                    <div class="flex flex-col border border-green-400 rounded-md w-full h-1/2">
-                        <div class="flex flex-row space-x-3 p-3 w-full items-center">
-                            <div class="text-green-400">
-                                <i class="fa-solid fa-user"></i>
-                            </div>
-                            <div>
-                                {{$event->user->name}}
-                            </div>
-                            <div class=" flex-grow flex flex-row justify-end text-white relative right-0">
-                                <div class=" bg-green-400 rounded-md p-2">
-                                    <p>Organizer</p>
->>>>>>> 48c93ae948f68424beb633f5f8ada041640f3a65
                                 </div>
                             </div>
                             <div class="flex flex-row space-x-3 p-3">
@@ -273,10 +189,9 @@
                                     <i class="fa-solid fa-star"></i>
                                 </div>
                                 <div>
-                                    <p>{{$event->interested}} is interested</p>
+                                    <p id="interestedNum">{{$event->interested}} is interested</p>
                                 </div>
                             </div>
-<<<<<<< HEAD
                             <div class="flex flex-row space-x-5 p-3">
                                 <div class="text-red-400">
                                     <i class="fa-solid fa-chair"></i>
@@ -284,37 +199,20 @@
                                 <div>
                                     <p>{{$event->no_of_seats}} seats are available </p>
                                 </div>
-=======
-                            <div>
-                                <p id="interestedNum">{{$event->interested}} is interested</p>
->>>>>>> 48c93ae948f68424beb633f5f8ada041640f3a65
                             </div>
                         </div>
                         <div class="flex flex-row">
                             @livewire('event-info-increment', ['event' => $event, 'user' => Auth::user()])
-                            {{--
-                            <livewire:event-info-increment :event="$event" /> --}}
                             {{-- <div
                                 class="w-1/3 p-2 text-center text-white rounded-md bg-orange-400 rounded-tr-none rounded-br-none">
-                                Interested
+                                <a onclick="interestedClick()">
+                                    Interested
+                                </a>
                             </div>
-<<<<<<< HEAD
                             <div
                                 class="w-2/3 p-2 text-center text-white rounded-md bg-red-400 rounded-tl-none rounded-bl-none">
                                 Going
                             </div> --}}
-=======
-                        </div>
-                    </div>
-                    <div class="flex flex-row">
-                        <div class="w-1/3 p-2 text-center text-white rounded-md bg-orange-400 rounded-tr-none rounded-br-none">
-                            <a onclick="interestedClick()">
-                                Interested
-                            </a>
-                        </div>
-                        <div class="w-2/3 p-2 text-center text-white rounded-md bg-red-400 rounded-tl-none rounded-bl-none">
-                            Going
->>>>>>> 48c93ae948f68424beb633f5f8ada041640f3a65
                         </div>
                     </div>
                 </div>
