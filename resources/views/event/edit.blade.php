f72c9ac163356c0d09d1d8547e7d86ef79d4a944
raychannudam <channudamray2002.rcu@gmail.com>
2022-08-10 19:27:37 +0700

    edit event

--- resources/views/event/edit.blade.php
+++ resources/views/event/edit.blade.php
@@ -5,27 +5,52 @@ $images = explode('|', $event->images)
 @section('content')
 @foreach ($event->images as $image)
 <img src="{{Storage::url($image->image)}}" width="70px" height="70px" alt="">
-<form action="/image/{{$image->id}}" method="POST">
-    <button type="submit">X</button>
+<form action="/image/{{$image->id}}" method="POST" id="cross-btn">
+    <button type="button" onclick="deleteImg()">
+        <i class="fa-solid fa-circle-xmark"></i>
+    </button>
     @csrf
     @method('DELETE')
 </form>
 @endforeach
-<form method="post" action="/event/{{$event->id}}" enctype="multipart/form-data">
-    <div class="input-group control-group increment">
-        <input type="text" name="title" placeholder="Event title" value="{{$event->title}}">
-        <input type="file" name="files[]" multiple class="form-control" value="{{$event->files}}">
-        <p>Images</p>
-        <input type="text" placeholder="description" name="description" value="{{$event->description}}">
-        <input type="number" placeholder="price" name="price" value="{{$event->price}}">
-        <input type="number" placeholder="no_of_seats" name="no_of_seats" value="{{$event->no_of_seats}}">
-        <input type="date" name="event_date" value="{{$event->event_date}}">
-        <div class="input-group-btn">
-            <button class="btn btn-success" type="button"><i class="glyphicon glyphicon-plus"></i>Add</button>
+<div class="h-screen">
+    <form method="post" action="/event/{{$event->id}}" enctype="multipart/form-data">
+        <div class="flex flex-col space-y-3  p-10 items-conter justify-center w-full">
+            <div class="text-center">
+                <input type="text" name="title" placeholder="Event title" value="{{$event->title}}" class="w-1/4 p-3 rounded-md">
+            </div>
+            <div class="text-center">
+                <input type="file" name="files[]" multiple class="form-control" value="{{$event->files}}" class="w-1/4 p-3 rounded-md">
+            </div>
+            <div class="text-center">
+                <input type="text" placeholder="description" name="description" value="{{$event->description}}" class="w-1/4 p-3 rounded-md">
+            </div>
+            <div class="text-center">
+                <input type="number" placeholder="price" name="price" value="{{$event->price}}" class="w-1/4 p-3 rounded-md">
+            </div>
+            <div class="text-center">
+                <input type="number" placeholder="no_of_seats" name="no_of_seats" value="{{$event->no_of_seats}}" class="w-1/4 p-3 rounded-md">
+            </div>
+            <div class="text-center">
+                <input type="date" name="event_date" value="{{$event->event_date}}" class="w-1/4 p-3 rounded-md">
+            </div>
+            <div class="input-group-btn">
+                <button class="btn btn-success" type="button"><i class="glyphicon glyphicon-plus"></i>Add</button>
+            </div>
         </div>
-    </div>
-    <button type="submit" class="btn btn-primary" style="margin-top:10px">Submit</button>
-    @csrf
-    @method('put')
-</form>
+        <button type="submit" class="btn btn-primary" style="margin-top:10px">Submit</button>
+        @csrf
+        @method('put')
+    </form>
+</div>
+<script>
+    function deleteImg(){
+        let isDelete = confirm('Are you sure?');
+        console.log(isDelete);
+        if(isDelete === true){
+            document.getElementById('cross-btn').submit();
+            console.log('OK')
+        }
+    }
+</script>
 @endsection
