07f2a99513b10782eadf37ad5da3520723b4eb68
raychannudam <channudamray2002.rcu@gmail.com>
2022-08-10 19:56:56 +0700

    edited forget password

--- resources/views/auth/forgetPassword.blade.php
+++ resources/views/auth/forgetPassword.blade.php
@@ -1,42 +1,43 @@
 @extends('app-layout.app')
 @section('content')
-<main class="login-form">
-    <div class="cotainer">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Reset Password</div>
-                    <div class="card-body">
-
-                        @if (Session::has('message'))
-                        <div class="alert alert-success" role="alert">
-                            {{ Session::get('message') }}
-                        </div>
-                        @endif
-
-                        <form action="{{ route('forget.password.post') }}" method="POST">
-                            @csrf
-                            <div class="form-group row">
-                                <label for="email_address" class="col-md-4 col-form-label text-md-right">E-Mail
-                                    Address</label>
-                                <div class="col-md-6">
-                                    <input type="text" id="email_address" class="form-control" name="email" required
-                                        autofocus>
-                                    @if ($errors->has('email'))
-                                    <span class="text-danger">{{ $errors->first('email') }}</span>
-                                    @endif
+<div class="p-10 w-full h-screen">
+    <main class="login-form">
+        <div class="cotainer">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="flex flex-col space-y-5 w-full items-center justify-center">
+                        <div class="card-header text-xl">Reset Password</div>
+                        <div class="card-body">
+    
+                            @if (Session::has('message'))
+                            <div class="alert alert-success" role="alert">
+                                {{ Session::get('message') }}
+                            </div>
+                            @endif
+    
+                            <form action="{{ route('forget.password.post') }}" method="POST">
+                                @csrf
+                                <div class="flex flex-col space-y-5">
+                                    <div>
+                                        <input type="text" id="email_address" class="p-3 outline-none rounded-md" name="email" required
+                                            autofocus placeholder="email address">
+                                        @if ($errors->has('email'))
+                                        <span class="text-danger">{{ $errors->first('email') }}</span>
+                                        @endif
+                                    </div>
+                                    <div class="bg-orange-500 text-white py-3 px-5 w-full rounded-md">
+                                        <button type="submit" class="btn btn-primary w-full">
+                                            Send Password Reset Link
+                                        </button>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
-                                </button>
-                            </div>
-                        </form>
-
+                            </form>
+    
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</main>
+    </main>
+</div>
+@endsection
